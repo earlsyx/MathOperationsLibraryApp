7def85b60e4b6e1f9f294c1a0825a60ebf952149
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-30 07:30:28 +0800

    Create test for the operations. aaa

---
+++ MathOperationsLibrary.Test/MathOperationsTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperationsLibrary.Test
+{
+    public class MathOperationsTests
+    {
+        [Fact]
+        public void MathOperationAddShouldReturnSum()
+        {
+            //Arrange
+            MathOperations operations = new MathOperations();
+            double expected = 13;
+
+            //Act
+            double actual = operations.Addition(6,7);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MathOperationAddShouldReturnDifference()
+        {
+            //Arrange
+            MathOperations operations = new MathOperations();
+            double expected = 150;
+
+            //Act
+            double actual = operations.Subtraction(200, 50);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MathOperationAddShouldReturnProduct()
+        {
+            //Arrange
+            MathOperations operations = new MathOperations();
+            double expected = 368;
+
+            //Act
+            double actual = operations.Multiplication(23, 16);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MathOperationAddShouldReturnQuotient()
+        {
+            //Arrange
+            MathOperations operations = new MathOperations();
+            decimal expected = 2;
+
+            //Act
+            decimal actual = operations.Division(20, 10);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+
+
+
+
+    }
+}
--- MathOperationsLibrary/MathOperations.cs
+++ MathOperationsLibrary/MathOperations.cs
@@ -9,24 +9,24 @@ namespace MathOperationsLibrary
     public class MathOperations
     {
         //Addition
-        public static double Addition(double x, double y)
+        public  double Addition(double x, double y)
         {
             return x + y;
         }
 
 
         //Subtraction
-        public static double Subtraction(double x, double y)
+        public  double Subtraction(double x, double y)
         {
             return x - y;
         }
         //Multiplication
-        public static double Multiplication(double x, double y)
+        public  double Multiplication(double x, double y)
         {
             return x * y;
         }
         //Division
-        public static float Division(float x, float y)
+        public  decimal Division(decimal x, decimal y)
         {
             return (x / y);
         }

864bd1b7d99ae68216a9e9f4e0fff12695187494
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-30 07:17:02 +0800

    *create class for common operations method * create xunit test proj

---
+++ MathOperationsLibrary/MathOperations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperationsLibrary
+{
+    public class MathOperations
+    {
+        //Addition
+        public static double Addition(double x, double y)
+        {
+            return x + y;
+        }
+
+
+        //Subtraction
+        public static double Subtraction(double x, double y)
+        {
+            return x - y;
+        }
+        //Multiplication
+        public static double Multiplication(double x, double y)
+        {
+            return x * y;
+        }
+        //Division
+        public static float Division(float x, float y)
+        {
+            return (x / y);
+        }
+    }
+}
